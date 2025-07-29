aa805915765aa4c57dbda01a6a7560e36519dff9
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2025-07-29 06:26:49 -0500

    simple

---
+++ friend.js
@@ -0,0 +1,10 @@
+let marks = 80;
+if (marks > 80) {
+  console.log("go for a lunch");
+} else if (marks < 80 && marks >= 60) {
+  console.log("good luck next time");
+} else if (marks < 60 && marks >= 40) {
+  console.log("massage unseen");
+} else {
+  console.log("block him");
+}
---
+++ ternary.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 8;
+
+let result;
+if (num1 > num2) {
+  result = num1 * 2;
+} else {
+  result = num1 + num2;
+}
+
+console.log(result);
