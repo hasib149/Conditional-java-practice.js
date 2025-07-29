04cb0dfea085aa85dbaafd6b07dd4324998dafda
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2025-07-29 05:05:02 -0500

    bmi calculator

---
+++ bmi.js
@@ -0,0 +1,10 @@
+const BMI = 42;
+if (BMI < 18.5) {
+  console.log("you are underweight");
+} else if (BMI >= 18.5 && BMI <= 24.9) {
+  console.log("you are normal");
+} else if (BMI >= 25 && BMI <= 29.9) {
+  console.log("you are overweight");
+} else {
+  console.log("you are obese");
+}
